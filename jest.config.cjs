4100d4393de123e46c157128bbcb0246cb79a1e9
Krupnov <pavelkm10@gmail.com>
2026-04-25 12:08:01 +0300

    test: configure jest and playwright coverage

---
+++ jest.config.cjs
@@ -0,0 +1,8 @@
+const { createCjsPreset } = require('jest-preset-angular/presets');
+
+module.exports = {
+  ...createCjsPreset(),
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  testEnvironment: 'jsdom',
+};
